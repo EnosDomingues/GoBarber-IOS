eb2485df1a9f090c13478736910b0d406a178ed4
Enos Domingues <enosdomingues@gmail.com>
2020-07-19 14:21:59 -0300

    Debugando app com Flipper

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   // baseURL: 'http://localhost:3333', // IOS URL
   // baseURL: 'http://10.0.2.2:3333', // Android URL
-  baseURL: 'http://172.20.10.2:3333', // Android / IOS IP local
+  baseURL: 'http://192.168.0.4:3333', // Android / IOS IP local
 });
 
 export default api;
