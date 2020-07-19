4a2f4bdf4658401f1004a95937a3ffda699c2ae5
Enos Domingues <enosdomingues@gmail.com>
2020-07-18 23:38:20 -0300

    Criando Picker de data

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  // baseURL: 'http://localhost:3333', // IOS URL
   // baseURL: 'http://10.0.2.2:3333', // Android URL
+  baseURL: 'http://192.168.0.4:3333', // Android / IOS IP local
 });
 
 export default api;
